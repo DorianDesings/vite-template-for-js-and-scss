9cbd6445c882ff37037e1157051f9d7d8302f7c8
Dorian <dorianoriginaldesings@gmail.com>
2024-02-22 08:54:21 +0100

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,2 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import catImage from '../assets/images/cat.jpeg';
 import '../scss/styles.scss';
-
-const img = document.createElement('img');
-img.src = catImage;
-
-document.body.append(img);
